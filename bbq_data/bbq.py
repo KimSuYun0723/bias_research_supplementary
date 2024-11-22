b97b74c19be37bd157ff7bc62e8b1d68a4abeeee
suyun kim <202200737@hufs.ac.kr>
2024-11-22 15:00:29 +0900

    Fix: bbq.py & Add: setting 1 dataset

--- bbq_data/bbq.py
+++ bbq_data/bbq.py
@@ -1,5 +1,4 @@
 # datasets pandas
-import pandas as pd
 from datasets import load_dataset
 
 def select_needed_columns(dataframe, selected_col):
@@ -18,68 +17,63 @@ def attach_disambig(dataframe, ambig_context_map):
     """
     'disambig'의 context에서 'ambig' context를 제거한 새 문장을 추가
     """
-    dataframe['disambig_minus_ambig'] = dataframe.apply(
-        lambda row: (
-            row['context'].replace(ambig_context_map.get(row['question'],""),"").strip()
-            if row['context_condition'] == 'disambig'
-            else None
-        ),
-        axis=1
-    )
+    def process_row(row):
+        if row['context_condition'] == 'disambig':
+            ambig_context = ambig_context_map[row['question']]
+            print("AMBIG:", ambig_context)
+            disambig_minus_ambig = row['context'].replace(ambig_context, "").strip()
+            print("D - A:", disambig_minus_ambig)
+            print()
+            # ambig context 제거
+            return disambig_minus_ambig
+        return None  # 'disambig'이 아닌 경우
+
+    dataframe['disambig_minus_ambig'] = dataframe.apply(process_row, axis=1)
     return dataframe
 
-def save_bbq_dataset(dataset_name, split, save_path, selected_col, setting=1):
+def save_bbq_dataset(dataset_name, save_path, selected_col, setting=1):
     """
     BBQ 데이터셋 처리 및 저장
 
     Args:
-        dataset_name (str) : "heegyu/bbq"
-        split (str) : standard dataset should be divided
+        dataset_name (str) : "seyoungsong/BBQ"
         save_path (str) : path final dataset(csv) will be saved
         selected_col (list) : list of columns
         setting (int) : 1 - basic processing, 2 - adding disambig context
     """
     print(f"=== LOADING BBQ DATASET... ===")
-    dataset = load_dataset(dataset_name, split=split, trust_remote_code=True)
-    df = pd.DataFrame(dataset)
-    negative_df = df[df["question_polarity"] == "negative"] # negative polarity만 추출한 df
+    dataset = load_dataset(dataset_name, trust_remote_code=True)
+    df = dataset['train'].to_pandas()
+    negative_df = df[df["question_polarity"] == "neg"] # negative polarity만 추출한 df
 
     print(f"=== SELECTING COLUMNS... ===")
     wanted_df = select_needed_columns(negative_df, selected_col)
     
     if setting==2:
         print(f"=== SETTING 2: ATTACHING COLUMN... ===")
-        ambig_context_map = negative_df[negative_df['context_condition']=='ambig'].set_index('question')['context'].to_dict()
+        # question : context
+        ambig_context_map = negative_df[(negative_df['context_condition']=='ambig') & (negative_df['category'] == 'Age')][:10]\
+            .set_index('question')['context'].to_dict()
         wanted_df = attach_disambig(wanted_df, ambig_context_map)
 
-    print(f"=== SAVING INTO CSV... ===")
+    """print(f"=== SAVING INTO JSONL... ===")
     #wanted_df.to_csv(save_path, index=False)
-    wanted_df.to_json(save_path, orient="records", lines=True)
+    wanted_df.to_json(save_path, orient="records", lines=True, force_ascii=False)"""
 
     print(f"=== SUCCESS! ===")
     return wanted_df
 
 # Setting 1
-data_name = "heegyu/bbq"
-save_path = "C:/_SY/bias_research/bbq_data/preprocessed_bbq/cleaned_bbq_set1.json"
-selected_columns = ["question_polarity", "categoty", "question", "context", "context_condition", ]
-
 save_bbq_dataset(
-    dataset_name=data_name,
-    split="test",
-    save_path=save_path,
-    selected_col=selected_columns
+    dataset_name= "seyoungsong/BBQ",
+    save_path= "C:/_SY/bias_research/bbq_data/preprocessed_bbq/cleaned_bbq_set1.jsonl",
+    selected_col=["question_polarity", "category", "question", "context", "context_condition"]
 )
 
 # Setting 2
-data_name = "heegyu/bbq"
-save_path = "C:/_SY/bias_research/bbq_data/preprocessed_bbq/cleaned_bbq_set2.json"
-selected_columns = ["question_polarity", "categoty", "question", "context", "context_condition", ]
-
-save_bbq_dataset(
-    dataset_name=data_name,
-    split="test",
-    save_path=save_path,
-    selected_col=selected_columns,
-    setting=2  # setting==2로 설정하여 새 열 추가
-)
+"""save_bbq_dataset(
+    dataset_name= "seyoungsong/BBQ",
+    save_path= "C:/_SY/bias_research/bbq_data/preprocessed_bbq/cleaned_bbq_set2.jsonl",
+    selected_col=["question_polarity", "category", "question", "context", "context_condition"],
+    setting=2
+)"""
